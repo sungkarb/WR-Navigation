d0262615aad4711ecf517e103b4841a15bab686a
Sungkar Bolat <145948463+sungkarb@users.noreply.github.com>
2024-10-17 19:09:00 -0500

    Added installation for Linux

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install python management packages
+sudo apt update
+sudo apt upgrade
+sudo apt-get install python3-pip
+sudo apt install python3-venv
+
+# Set the virtual environment directory
+VENV_DIR="robotics"
+
+# Check if the virtual environment directory exists
+if [ -d "$VENV_DIR" ]; then
+    echo "Virtual environment already exists."
+else
+    echo "Creating virtual environment..."
+    python3 -m venv $VENV_DIR
+    echo "Activating virtual environment..."
+    source "$VENV_DIR/bin/activate"
+
+    # Install required packages
+    pip install matplotlib
+    pip install numpy
+    pip install laspy
+    pip install lazrs
+fi
+
+# Activate the virtual environment if it was created or exists
+source "$VENV_DIR/bin/activate"
+
+# Keep the terminal open
+read -p "Press Enter to exit"
