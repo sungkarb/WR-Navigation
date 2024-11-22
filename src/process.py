fbad0a7fbcef16465ee8ba3cf17456e15554f2b3
Isaac <isaacchu888@gmail.com>
2024-11-21 19:39:45 -0600

    box processor fix

--- src/process.py
+++ src/process.py
@@ -1,15 +1,26 @@
+'''
+Script that processes new csv files and applies offsets to the x, y, and z columns, then saves the new csv files.
+Also randomizes and drops duplicates from the new csv files.
+'''
+# input the name of the csv file to process
+box_name = "box1"
+#
+
 import pandas as pd
+import os
 import json
-try:
-    box = pd.read_csv("data/box1_old.csv", index_col=0)
-except FileNotFoundError:
-    raise FileNotFoundError("rename box1.csv to box1_old.csv")
 
 with open("settings.json", "r") as f:
     parameters = json.load(f)
+    data_dir = parameters['data_dir']
     x_offset = parameters['x_offset']
     y_offset = parameters['y_offset']
     z_offset = parameters['z_offset']
+
+try:
+    box = pd.read_csv(os.path.join(data_dir, f"{box_name}_old.csv"), index_col=0)
+except FileNotFoundError:
+    raise FileNotFoundError(f"rename {box_name}.csv to {box_name}_old.csv")
 
 # rename the header to "x", "y", "z"
 box.columns = ["x", "y", "z"]
@@ -19,6 +30,6 @@ box['x'] -= x_offset
 box['y'] -= y_offset
 box['z'] -= z_offset
 print(box.head())
-box.to_csv("data/box1.csv", index=False)
+box.to_csv(os.path.join(data_dir, f"{box_name}.csv"), index=False)
 
-box.sample(frac=1).reset_index(drop=True).to_csv("data/box1_random.csv", index=False)
+box.sample(frac=1).reset_index(drop=True).to_csv(os.path.join(data_dir, f"{box_name}_random.csv"), index=False)
