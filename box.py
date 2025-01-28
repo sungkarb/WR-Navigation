acb0fa4bbaad8a454762983e099fab24a95b6a05
Isaac <isaacchu888@gmail.com>
2025-01-27 19:42:56 -0600

    box processing script

--- box.py
+++ box.py
@@ -5,7 +5,7 @@ Processes all of the boxes so that everything else can run
 import os
 import json
 import pandas as pd
-import laspy, lazrs
+import laspy
 import numpy as np
 
 print("Starting . . .")
@@ -17,17 +17,21 @@ with open(settings_path, "r") as f:
     boxes_csv_dir = parameters["boxes_csv_dir"]
     boxes_random_dir = parameters["boxes_random_dir"]
 
-print("Proessing Boxes (this will take a long time). . .")
+boxes_raw_dir = os.path.join("map_data", boxes_raw_dir)
+boxes_csv_dir = os.path.join("map_data", boxes_csv_dir)
+boxes_random_dir = os.path.join("map_data", boxes_random_dir)
+
+print("Proessing 12 Boxes (this will take a long time). . .")
 for i in range(1, 13):
     print(f"\tBox {i}")
     raw_box_name = f"box{i}.laz"
     box_name = f"box{i}.csv"
     raw_box_path = os.path.join(boxes_raw_dir, raw_box_name)
-    las = laspy.read(box_path)
+    las = laspy.read(raw_box_path)
     raw_box = pd.DataFrame(np.vstack((las.x, las.y, las.z)).transpose())
     raw_box.columns = ["x", "y", "z"]
     box = raw_box.sort_values(by=['x', 'y', 'z'])
     box.reset_index(drop=True)
     box_path = os.path.join(boxes_csv_dir, box_name)
-    box.to_csv(box_name, index=False)
+    box.to_csv(os.path.join(boxes_csv_dir, box_name), index=False)
     box.sample(frac=1).reset_index(drop=True).to_csv(os.path.join(boxes_random_dir, box_name), index=False)
