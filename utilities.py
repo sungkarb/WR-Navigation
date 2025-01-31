6aed06ac62a8d5f2b5aaae28662f32f02adcb785
Sungkar Bolat <145948463+sungkarb@users.noreply.github.com>
2025-01-30 19:30:37 -0600

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,3 +1,11 @@
+import laspy, os
+import numpy as np
+import pandas as pd
+from pyproj import Transformer
+
+GPS_CRS="EPSG:4326"
+POINT_CRS="EPSG:32612"
+
 """
 This function converts the given points to GPS coordinates.
 param points: The points to convert.
@@ -5,6 +13,36 @@ param source_crs: The source coordinate reference system (CRS).
 
 return: np.ndarray: The GPS coordinates.
 """
-def to_gps(points: np.ndarray, source_crs="EPSG32612") -> np.ndarray:
-    transformer = Transformer.from_crs(source_crs, "EPSG:4326", always_xy=True)
-    return np.array([transformer.transform(x, y, z) for x, y, z in points])
+def to_gps(points: np.ndarray, source_crs=POINT_CRS) -> np.ndarray:
+    transformer = Transformer.from_crs(source_crs, GPS_CRS, always_xy=True)
+    result = np.array([transformer.transform(x, y, z) for x, y, z in points])
+    result[:, [0, 1]] = result[:, [1, 0]]
+    return result
+
+
+"""
+This function converts the laz file to the dataframe of gps coordinates
+and saves it in csv
+
+Args:
+    box_file: path to the box file which must be laz file
+"""
+def box_to_gps(box_file: str) -> np.ndarray:
+    result_name = box_file + ".csv"
+    with laspy.open(box_file) as f:
+        las = f.read()
+
+    points = np.vstack((las.x, las.y, las.z)).transpose()
+    crs = las.header.parse_crs()
+    if crs:
+        print("Coordinate Reference System (CRS):", crs)
+    else:
+        crs = POINT_CRS
+
+    transformer = Transformer.from_crs(crs, GPS_CRS, always_xy=True)
+    gps_cords = np.array([transformer.transform(x, y, z) for x, y, z in points])
+    gps_cords[:, [0, 1]] = gps_cords[:, [1, 0]]
+
+    df = pd.DataFrame(gps_cords, columns=["Latitude", "Longtitude", "Altitude"])
+    df.to_csv(result_name, index=False)
+    print(f"GPS coordinates saved to {result_name}")
